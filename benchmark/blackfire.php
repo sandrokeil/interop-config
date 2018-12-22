e144c9d2ae83e1779584a9c57843e757f1859b8b
Sandro Keil <github@sandro-keil.de>
2018-12-22 11:58:10 +0100

    It's 2018

--- benchmark/blackfire.php
+++ benchmark/blackfire.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Sandro Keil (https://sandro-keil.de)
+ *
+ * @link      http://github.com/sandrokeil/interop-config for the canonical source repository
+ * @copyright Copyright (c) 2015-2018 Sandro Keil
+ * @license   http://github.com/sandrokeil/interop-config/blob/master/LICENSE.md New BSD License
+ */
 
 chdir(dirname(__DIR__));
 
--- test/Tool/ConfigDumperCommandTest.php
+++ test/Tool/ConfigDumperCommandTest.php
@@ -3,7 +3,7 @@
  * Sandro Keil (https://sandro-keil.de)
  *
  * @link      http://github.com/sandrokeil/interop-config for the canonical source repository
- * @copyright Copyright (c) 2017-2017 Sandro Keil
+ * @copyright Copyright (c) 2017-2018 Sandro Keil
  * @license   http://github.com/sandrokeil/interop-config/blob/master/LICENSE.md New BSD License
  */
 
@@ -97,7 +97,7 @@ class ConfigDumperCommandTest extends TestCase
  * Sandro Keil (https://sandro-keil.de)
  *
  * @link      http://github.com/sandrokeil/interop-config for the canonical source repository
- * @copyright Copyright (c) 2017-2017 Sandro Keil
+ * @copyright Copyright (c) 2017-2018 Sandro Keil
  * @license   http://github.com/sandrokeil/interop-config/blob/master/LICENSE.md New BSD License
  */
 
